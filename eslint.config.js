b663785d030b53dd8cb6d0eafd8de571d7d960b5
Ikuomola Stephen <cstephenid@yahoo.com>
2025-03-07 17:05:11 +0100

    modify(core): add eslint config file for jsdoc

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,24 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import jsdoc from 'eslint-plugin-jsdoc';
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
+  eslintPluginPrettierRecommended,
+  eslintConfigPrettier,
+  jsdoc.configs['flat/recommended'],
+  {
+    ignores: ['node_modules/**', '**/dist', '.parcel-cache/**']
+  },
+  {
+    files: ['**/*.js'],
+    plugins: {
+      jsdoc
+    },
+  }
 ];
